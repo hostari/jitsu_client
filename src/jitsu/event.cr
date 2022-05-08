bd4b6f40dfb181c6baab248e3110d1b15a40481c
Xavi Ablaza <25378966+xaviablaza@users.noreply.github.com>
2022-05-08 16:24:38 +0800

    Use string as body
    Add documentation to methods

--- src/jitsu/event.cr
+++ src/jitsu/event.cr
@@ -1,6 +1,8 @@
 module Jitsu
   class Event
-    def self.create(body : Hash(String, String), client : Client)
+    # Creates a Jitsu event.
+    # See: https://jitsu.com/docs/sending-data/api
+    def self.create(body : String, client : Client)
       client.post("/api/v1/s2s/event", body: body)
     end
   end
